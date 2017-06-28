7fa6d23a6d32d8526077b385ea4ae069bf3b3cc2
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2017-06-27 19:14:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Smart Calculator with python
+
+Programmed by <em>Python 2.7</em>
+
+
+This is a smart calculator allow user to enter a string of mathematical operations <em>"+ or - or * or / or spaces"<em/>, without <em>(brackets)</em>
+
+# Test
+Enter in your terminal (cmd in windows) and try this code :
+
+
+$ <b>python smart_calc.py</b>
+
+$ <b>88 + 10 -15 * 20 * 3 / 10 / 2 - 45</b>
+
+
+The result :
+
+$ <b>------</b>
+
+$ <b>8.0</b>
+
+$ <b>------</b>
+
+
+![Screenshot](screen_shot.png)
+
+
+
+
+# Explanation :
+
+i used Regex <em>(Regular Expression)</em> :
+
+^ : begin of expression
+
+$ : end of expression
+
+[-+*/] : '-' or '+' or '*' or '/'
+
+[0-9] : 1 2 3 ... 9
+
+(?:) : non-capturing group
+
+? : 1 time or not
+
+* : 0 or more times
+
++ : 1 or more times
+
+<em> for more informations about Regex, visit </em> <a href='https://en.wikipedia.org/wiki/Regular_expression#Standards'>wiki regex</a>
+
+
+
+
+# Bidmas :
+
+Bidmas is an order of operation used when doing equations, which include both addition and subtraction, or Multiplication and Division.
+
+This is the order it goes in :
+
+[B] <em>Brackets</em>
+
+[I] <em>Indicies</em>
+
+[D] <em>Division</em>
+
+[M] <em>Multiplication</em>
+
+[A] <em>Addition</em>
+
+[S] <em>Subtraction</em>
+
+
+
+
+:: zakaria chahboun | 2017 ::
---
+++ smart_calc.py
@@ -0,0 +1,81 @@
+
+import re # regex : regular expressions
+
+# main
+def main():
+
+	print('Enter your mathematical operations: ')
+	# Example : 88 + 10 -15 * 20 * 3 / 10 / 2 - 45
+	# = 8
+
+	Input = raw_input() # String
+
+	#---- validate mathematical expressions ----
+	if re.search('^\s*([0-9]+)(?:\s*([-+*/])\s*((?:\s[-+])?[0-9]+)\s*)+$',Input):
+
+		# return a list of operations, for example : ['10','+','4','/','8']
+		All = re.findall("[-*/+]+|[0-9]+", Input)
+
+		#---- Division ----
+		while '/' in All:
+			i = All.index('/')
+
+			# convert String to float and calculat the division
+			div = float(All[i-1])/float(All[i+1])
+			# delete the operation for example : ['10','/','2']
+			del All[i-1] # delete : ['10']
+			del All[i-1] # delete : ['/']
+			del All[i-1] # delete : ['2']
+			All.insert(i-1,div) # replace [ 10 / 2 ] with 5
+
+		#---- Multiplication ----
+		while '*' in All:
+			i = All.index('*')
+			mult = float(All[i-1])*float(All[i+1])
+			del All[i-1]
+			del All[i-1]
+			del All[i-1]
+			All.insert(i-1,mult)
+
+		#---- Addition or Substraction ----
+		while '+' in All or '-' in All:
+			i = 1 # number 2, for example : ['1','+','2'] so : All[1] == '+'
+
+			if All[i] == '+':
+				add = float(All[i-1])+float(All[i+1])
+				del All[i-1]
+				del All[i-1]
+				del All[i-1]
+				All.insert(i-1,add)
+
+			elif All[i] == '-':
+				add = float(All[i-1])-float(All[i+1])
+				del All[i-1]
+				del All[i-1]
+				del All[i-1]
+				All.insert(i-1,add)
+
+
+		#---- The Result ----
+		print('------')
+		print(All[0])
+		print('------')
+
+	#---- Errors! ----
+	else:
+		print('OPPS !! wrong expressions!')
+
+	
+	
+# Start here	
+if __name__ == '__main__': main()
+
+
+#-----------------
+# python 2.7
+
+# Smart Calculator by zakaria chahboun | 27/06/2017
+
+# twitter : @zaki_chahboun
+# facebook : /zakaria.chahboun.2018
+#-----------------
